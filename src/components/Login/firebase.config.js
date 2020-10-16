20147645bd230b70b55d7e01d116acde8d7ef572
RiajulpHero <67540508+RiajulpHero@users.noreply.github.com>
2020-10-16 12:01:59 +0600

    burj-al-arab-with-GoogleLogIn

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDts4vDmsxaX-w6rn5oX5kMrRnFC07NyRU",
+    authDomain: "burj-al-arab-ri.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-ri.firebaseio.com",
+    projectId: "burj-al-arab-ri",
+    storageBucket: "burj-al-arab-ri.appspot.com",
+    messagingSenderId: "479841219301",
+    appId: "1:479841219301:web:d9c281fa612e05108d5a0c"
+  };
+  export default firebaseConfig;
